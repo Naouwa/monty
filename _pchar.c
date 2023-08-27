bf9b71010ced8cd25cbd69727122addaa3ec5e7a
Naouwa <densali.nahla@gmail.com>
2023-08-27 20:40:53 +0100

    changed pchar

--- _pchar.c
+++ _pchar.c
@@ -14,11 +14,10 @@ void pchar(stack_t **head, unsigned int line_num)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
 	}
-	if (h->n > 127 || h->n < 0)
+	if (h->n < 32 || h->n > 126)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_num);
 		exit(EXIT_FAILURE);
 	}
-
 	printf("%c\n", h->n);
 }
